a979efd7f5c3b757f7677b639b3a7384721a9b5c
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-01 15:44:26 -0300

    Improve desing on ChatInput Component

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -2,8 +2,8 @@ import { ArrowUp, Plus, Globe, Lightbulb, Ellipsis } from 'lucide-react'
 
 export default function ChatInput() {
   return (
-    <div className='w-full rounded-2xl border-1 border-black px-4 py-2'>
-      <div>
+    <div className='w-full rounded-2xl border-1 border-gray-200 px-4 py-2'>
+      <div className='mb-2'>
         <input
           className='w-full p-2 focus-visible:outline-none'
           type='text'
@@ -15,23 +15,23 @@ export default function ChatInput() {
       <div className='flex justify-between'>
         <div className='flex gap-1'>
           <div>
-            <button className='rounded-full border-1 border-black p-2'>
-              <Plus color='black' />
+            <button className='rounded-full border-1 border-gray-200 p-2'>
+              <Plus color='#4a5565' />
             </button>
           </div>
           <div>
-            <button className='rounded-full border-1 border-black p-2'>
-              <Globe color='black' />
+            <button className='rounded-full border-1 border-gray-200 p-2'>
+              <Globe color='#4a5565' />
             </button>
           </div>
           <div>
-            <button className='rounded-full border-1 border-black p-2'>
-              <Lightbulb color='black' />
+            <button className='rounded-full border-1 border-gray-200 p-2'>
+              <Lightbulb color='#4a5565' />
             </button>
           </div>
           <div>
-            <button className='rounded-full border-1 border-black p-2'>
-              <Ellipsis color='black' />
+            <button className='rounded-full border-1 border-gray-200 p-2'>
+              <Ellipsis color='#4a5565' />
             </button>
           </div>
         </div>
